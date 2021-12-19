432a64fce144f22dff14fc9d90717a4fd2576e38
Amanda Katariny <amandakata@outlook.com>
2021-12-19 00:16:53 -0300

    Aula 01 - Novos Produtores e Consumidores

--- service-new-order/src/main/java/br/com/alura/ecommerce/Order.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/Order.java
@@ -4,12 +4,14 @@ import java.math.BigDecimal;
 
 public class Order {
 
-    private final String userId, orderId;
+    private final String orderId;
+    private final String email;
     private final BigDecimal amount;
 
-    public Order(String userId, String orderId, BigDecimal amount) {
-        this.userId = userId;
+    public Order(String orderId, BigDecimal amount, String email) {
+
         this.orderId = orderId;
         this.amount = amount;
+        this.email = email;
     }
 }
